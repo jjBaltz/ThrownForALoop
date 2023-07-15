6db6fb72da00633a8701ff671c39eae99f3cbc15
Jasper Baltz <nickolas.baltz@gmail.com>
2023-07-15 10:22:23 -0500

    thrownloop_first

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+
+List<Product> products = new List<Product>()
+{
+    new Product()
+    { 
+        Name = "Football", 
+        Price = 15.00M, 
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010,
+        Condition = 2.5
+    },
+    new Product() 
+    { 
+        Name = "Hockey Stick", 
+        Price = 12.00M, 
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010,
+        Condition = 4.2
+    }
+};
+
+Console.WriteLine(greeting);
+
+decimal totalValue = 0.0M;
+foreach (Product product in products)
+{
+    if (!product.Sold)
+    {
+        totalValue += product.Price;
+    }
+}
+Console.WriteLine($"Total inventory value: ${totalValue}");
+
+Console.WriteLine("Products:");
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
+}
+Console.WriteLine("Please enter a product number: ");
+
+int response = int.Parse(Console.ReadLine().Trim());
+
+while (response > products.Count || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
+}
+
+DateTime now = DateTime.Now;
+
+Product chosenProduct = products[response - 1];
+
+TimeSpan timeInStock = now - chosenProduct.StockDate;
+
+Console.WriteLine(@$"You chose: 
+{chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+It is {now.Year - chosenProduct.ManufactureYear} years old. 
+It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}
+It is in {chosenProduct.Condition} condition.");
